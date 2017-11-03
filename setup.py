9c5e0653038c7543312f6fa62c9a85e8bc02bb42
Пушкарев Александр Сергеевич <pushkarev@proitr.ru>
2017-11-03 16:39:45 +0500

    Added pip

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='transperm',
     version='1.0.0',
-    packages=find_packages(),
+    packages=['transperm'],
     author='Alexander Pushkarev',
     author_email='axp-dev@yandex.com',
     description='Библиотека для получения расписания транспорта г. Перми.',
     license='MIT',
     keywords='transperm perm api',
     url='https://github.com/axp-dev/py-transperm-api',
-    install_requires=['requests']
+    install_requires=['requests'],
+    classifiers=[],
 )
